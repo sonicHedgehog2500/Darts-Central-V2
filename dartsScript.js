946e606884833b98ae79fe2f22bfd8543067d7cb
sonicHedgehog2500 <48524624+sonicHedgehog2500@users.noreply.github.com>
2021-02-08 17:59:12 +0000

    Add files via upload

---
+++ dartsScript.js
@@ -0,0 +1,10 @@
+	
+	/*****Displays A Maintenance Message*****/
+	function maintenanceAlert(){
+		alert("This content is currently undergoing maintenance and will be fully functioning soon.");
+	}
+	
+	/*****Displays The Disclaimer*****/
+	function viewDisclaimer(){
+		alert("This site has been constructed as coursework for module U08054 Web Technology, at Oxford Brookes University. It is not a working website and is not connected with any site or individual referenced. The views and opinions expressed within these pages are personal and should not be construed as reflecting the views and opinions of Oxford Brookes University.");
+	}
